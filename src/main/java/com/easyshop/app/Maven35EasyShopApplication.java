0b82f2d32d20c2c705ee25030928307061dbad85
Yogesh-31B <yogesh.b.bhagat1@gmail.com>
2024-06-06 21:41:41 +0530

    fist sb project

---
+++ src/main/java/com/easyshop/app/Maven35EasyShopApplication.java
@@ -0,0 +1,13 @@
+package com.easyshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Maven35EasyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Maven35EasyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easyshop/app/Maven35EasyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyshop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Maven35EasyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
